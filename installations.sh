8111a0f3632ff6637968a275fc35c2b5a9408846
Tanchwa <89235018+Tanchwa@users.noreply.github.com>
2022-10-14 10:24:39 -0400

    Update installations.sh

--- installations.sh
+++ installations.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# install homebrew
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+#upgrade bash
+brew upgrade && brew install bash
+
+#vim stuff
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
