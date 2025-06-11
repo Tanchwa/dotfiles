1ebfbad2decf46f8d132174ce027cfedd469635e
Tanchwa <dres2dogs@gmail.com>
2025-06-10 20:37:19 -0400

    updated icons

--- .config/waybar/custom_modules/nvidia_gpu.sh
+++ .config/waybar/custom_modules/nvidia_gpu.sh
@@ -4,4 +4,4 @@ temperature=$(nvidia-smi --query-gpu=temperature.gpu | sed -n '2p')
 deviceinfo=$(nvidia-smi --query-gpu=gpu_name | sed -n '2p')
 driverinfo=$()
 
-echo '{"text": "  '$temperature'°C", "class": "custom-gpu", "tooltip": "<b>'$deviceinfo'</b>\n'$driverinfo'"}'
+echo '{"text": "  '$temperature'°C", "class": "custom-gpu", "tooltip": "<b>'$deviceinfo'</b>\n'$driverinfo'"}'
