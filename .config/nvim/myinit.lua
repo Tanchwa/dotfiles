c8241b5cc3b53882df68b0f2df5ab532fe8705f0
Tanchwa <dres2dogs@gmail.com>
2025-08-10 09:39:15 -0400

    fixed lsp settings for yaml files

--- .config/nvim/myinit.lua
+++ .config/nvim/myinit.lua
@@ -89,27 +89,46 @@ local cmp = require'cmp'
     capabilities = capabilities
   }
   require('lspconfig')['yamlls'].setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+    cmd = { "/opt/yaml-language-server/bin/yaml-language-server", "--stdio" },
     settings = {
-        yaml = {
-          schemas = {
-            kubernetes = "*.yaml",
-            ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-            ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-            ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-            ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-            ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-            ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-            ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-            ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-            ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-            ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-            ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-            ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+      redhat = { telemetry = { enabled = false } },
+      yaml = {
+        completion = true,
+        validate = true,
+        hover = true,
+        format = {
+          enable = true,
+        },
+        schemaStore = {
+          enable = true,
+          url = "",
+        },
+        schemas = {
+          kubernetes = "*.yaml",
+          ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+          ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+          ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+          ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+          ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+          ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+          ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+          ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+          ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+          ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+          ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+          ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+          ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
+                  "/azure-pipeline*.y*l",
+                  "/*.azure*",
+                  "Azure-Pipelines/**/*.y*l",
+                  "Pipelines/*.y*l",
+            },
           },
         },
       },
     }
+
 --  require('lspconfig')['azure_pipelines_ls'].setup {
 --    settings = {
 --      yaml = {
