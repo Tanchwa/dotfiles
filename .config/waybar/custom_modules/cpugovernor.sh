eb1885bdddbff0b476443ce6a9e8e6f1715fe813
Tanchwa <dres2dogs@gmail.com>
2026-05-21 12:26:04 +0800

    changes to some dotfiles after upgrading to fedora 44

--- .config/waybar/custom_modules/cpugovernor.sh
+++ .config/waybar/custom_modules/cpugovernor.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-GOVERNOR=$(asusctl profile -p --help | sed -n '2p' | sed 's/Active\ profile\ is\ //')
+GOVERNOR=$(asusctl profile get | head -n 1 | awk '{print $3}')
 
 if [ $GOVERNOR = Performance ]; then
 	echo '{"text": "perf", "alt": "perf", "class": "performance", "tooltip": "<b>Governor</b> Performance"}'
 	if [[ $1 = switch ]]; then
-		asusctl profile -P Balanced;pkill -RTMIN+8 waybar;
+		asusctl profile set Balanced;pkill -RTMIN+8 waybar;
 	fi
 	#echo ''
 elif [ $GOVERNOR = Balanced ]; then
 	echo '{"text": "balanced", "alt": "balanced", "class": "balanced", "tooltip": "<b>Governor</b> Balanced"}'
 	if [[ $1 = switch ]]; then
-		asusctl profile -P Quiet;pkill -RTMIN+8 waybar;
+		asusctl profile set Quiet;pkill -RTMIN+8 waybar;
 	fi
 elif [ $GOVERNOR = Quiet ]; then
 	echo '{"text": "quiet", "alt": "quiet", "class": "balanced", "tooltip": "<b>Governor</b> Quiet"}'
 	if [[ $1 = switch ]]; then
-		asusctl profile -P Performance;pkill -RTMIN+8 waybar;
+		asusctl profile set Performance;pkill -RTMIN+8 waybar;
 	fi
 fi
--- bin/light_dark_switcher.sh
+++ bin/light_dark_switcher.sh
@@ -7,12 +7,14 @@ case "$1" in
         gsettings set org.gnome.desktop.interface gtk-theme "Gruvbox-Light"
         gsettings set org.gnome.desktop.interface icon-theme "Gruvbox-Plus-Light"
         folders-color-chooser.sh --color=rust
+        swww img ~/Pictures/Wallpapers/gruvbox_light/gruvbox_1.gif
         ;;
         --dark)
         gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
         gsettings set org.gnome.desktop.interface gtk-theme "Gruvbox-Dark"
         gsettings set org.gnome.desktop.interface icon-theme "Gruvbox-Plus-Dark"
         folders-color-chooser.sh --color=highland
+        swww img ~/Pictures/Wallpapers/gruvbox_dark/wallhaven-2e2xyx.jpg
         ;;
         *)
         echo "Usage: ${0##*/} [--light|--dark]"
